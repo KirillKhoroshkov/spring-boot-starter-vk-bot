0a5bb43814d7f0879e7dee61ff45315f14921928
KirillKhoroshkov <khoroshkov_kirill@mail.ru>
2020-09-25 01:21:12 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/springbootstartervkbot/SpringBootStarterVkBotApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootstartervkbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootStarterVkBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootStarterVkBotApplication>(*args)
+}
